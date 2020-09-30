00f340be1841f58aac8013e0ca34eee17614d819
Hafidz Mahrus <havus.it@gmail.com>
2020-10-01 00:28:30 +0800

    add comment following table user columns

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+# This model has the following table columns:
+# t.bigint "twitter_user_id"
+# t.string "screen_name"
+# t.string "name"
+# t.string "profile_image_url"
+# t.string "oauth_token"
+# t.string "oauth_token_secret"
+# t.integer "classification", default: 0
+# t.integer "ticket_remaining", default: 0
+
 class User < ApplicationRecord
   # from audited gem for versioning
   audited
